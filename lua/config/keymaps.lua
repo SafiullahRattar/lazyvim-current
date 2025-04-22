9314db0169092437efedbf0754c7194029b4a7a4
SafiullahRattar <safiullahrattar313@gmail.com>
2025-04-22 16:54:33 +0500

    updated useful keymap

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -8,3 +8,73 @@ vim.keymap.set("n", "<C-d>", "<C-d>zz")
 vim.keymap.set("n", "<C-u>", "<C-u>zz")
 vim.keymap.set("n", "<leader>ac", "<cmd>CodeCompanionChat Toggle<cr>", { desc = "Code Companion Chat" })
 vim.keymap.set("n", "<leader>ai", "<cmd>CodeCompanionActions<cr>", { desc = "Code Companion Actions" })
+
+local function swap_path_line()
+  -- Path mappings (without requiring the exact path with trailing slash)
+  local paths = {
+    ["/home/safi/safihasanfaraz%-share"] = "/home/hassan/sharefiles-text",
+    ["/home/hassan/sharefiles%-text"] = "/home/safi/safihasanfaraz-share",
+  }
+
+  -- Get the current line
+  local line = vim.api.nvim_get_current_line()
+  local new_line = line
+
+  -- Try each path mapping
+  for from_path, to_path in pairs(paths) do
+    if line:find(from_path) then
+      new_line = line:gsub(from_path, to_path)
+      break
+    end
+  end
+
+  -- Update the current line if a replacement was made
+  if new_line ~= line then
+    vim.api.nvim_set_current_line(new_line)
+    print("Path swapped!")
+  else
+    print("No matching path found on this line.")
+  end
+end
+
+local function swap_paths_file()
+  -- Path mappings (without requiring the exact path with trailing slash)
+  local paths = {
+    ["/home/safi/safihasanfaraz%-share"] = "/home/hassan/sharefiles-text",
+    ["/home/hassan/sharefiles%-text"] = "/home/safi/safihasanfaraz-share",
+  }
+
+  -- Get all lines in the buffer
+  local lines = vim.api.nvim_buf_get_lines(0, 0, -1, false)
+  local changes_made = false
+
+  for i, line in ipairs(lines) do
+    local new_line = line
+
+    -- Try each path mapping on this line
+    for from_path, to_path in pairs(paths) do
+      if line:find(from_path) then
+        new_line = line:gsub(from_path, to_path)
+        changes_made = true
+        break
+      end
+    end
+
+    -- Update the line if changes were made
+    if new_line ~= line then
+      lines[i] = new_line
+    end
+  end
+
+  -- Update the buffer with modified lines
+  if changes_made then
+    vim.api.nvim_buf_set_lines(0, 0, -1, false, lines)
+    print("All paths in file swapped!")
+  else
+    print("No matching paths found in file.")
+  end
+end
+
+-- Add your keymaps here
+vim.keymap.set("n", "<leader>an", swap_path_line, { noremap = true, desc = "Swap file path on current line" })
+vim.keymap.set("n", "<leader>aN", swap_paths_file, { noremap = true, desc = "Swap all file paths in the entire file" })
