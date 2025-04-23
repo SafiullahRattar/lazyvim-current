b220afb16140e6e1211b294c30dbba37915d1a79
SafiullahRattar <safiullahrattar313@gmail.com>
2025-04-23 15:13:57 +0500

    oil copy file path function

--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -89,7 +89,46 @@ end
 vim.keymap.set("x", "<leader>cy", ":CopyOilContents<CR>", { desc = "Copy selected file contents" })
 -- Normal mode mapping to open Oil (parent directory)
 vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
+local function copy_selected_file_paths(start_line, end_line)
+  local oil = require("oil")
+  local oil_dir = oil.get_current_dir(0)
 
+  -- Fallback to current buffer's directory if Oil's directory isn't available
+  if not oil_dir or oil_dir == "" then
+    oil_dir = vim.fn.expand("%:p:h") .. "/"
+  end
+
+  local lines = vim.api.nvim_buf_get_lines(0, start_line - 1, end_line, false)
+  if not lines or #lines == 0 then
+    print("No files selected. Try again.")
+    return
+  end
+
+  local file_paths = {}
+  for _, line in ipairs(lines) do
+    local filename = line:match("[^%s]+$")
+    if filename then
+      local filepath = oil_dir .. filename
+      table.insert(file_paths, filepath)
+    end
+  end
+
+  if #file_paths > 0 then
+    local final_content = table.concat(file_paths, "\n")
+    vim.fn.setreg("+", final_content)
+    print("Copied " .. #file_paths .. " file path(s) to clipboard")
+  else
+    print("No valid files found. Try again.")
+  end
+end
+
+-- Create a user command for copying file paths
+vim.api.nvim_create_user_command("CopyOilPaths", function(opts)
+  copy_selected_file_paths(opts.line1, opts.line2)
+end, { range = true })
+
+-- Visual mode mapping for copying file paths
+vim.keymap.set("x", "<leader>cp", ":CopyOilPaths<CR>", { desc = "Copy selected file paths" })
 return {
   "stevearc/oil.nvim",
   ---@module 'oil'
